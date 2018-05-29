b162e59de996f58b7de01ff35a6135604974f01c
Aaron Hopkins <github@die.net>
2018-05-28 21:26:06 -0700

    Initial import.

---
+++ leakybucket.go
@@ -0,0 +1,172 @@
+package leakybucket
+
+import (
+	"hash/fnv"
+	"sync"
+	"time"
+)
+
+const (
+	// DrainPerSecond is the number of tokens per second drained from each bucket
+	DrainPerSecond      = 1000000000 
+	gcScanEntries       = 1000
+	gcMustRemoveEntries = 100
+)
+
+type entry struct {
+	tokens  int64 // one billion drained per second
+	updated int64 // unix nanoseconds
+}
+
+// Cache is a size-limited hash-map implementation of the leaky
+// bucket rate-limiting algorithm. Entries are roughly 78 bytes each.
+type Cache struct {
+	MaxEntries int
+
+	mu      sync.Mutex
+	cache   map[uint64]entry
+	entries int
+}
+
+// New creates a Cache capable of storing up to maxEntries entries.
+func New(maxEntries int) *Cache {
+	if maxEntries <= 0 {
+		return nil
+	}
+
+	b := &Cache{
+		MaxEntries: maxEntries,
+		cache:      make(map[uint64]entry),
+	}
+
+	return b
+}
+
+// Put attempts to add quantity tokens into the bucket referred to by a
+// uint64 key that has a size limit of limit tokens.  Return the current
+// number of tokens in the bucket, whether the key already existed, and
+// whether there was enough room in the bucket to add the requested number
+// of tokens.
+func (lb *Cache) Put(key uint64, quantity, limit int64) (int64, bool, bool) {
+	now := time.Now().UnixNano()
+
+	lb.mu.Lock()
+	tokens, exists, ok := lb.put(key, quantity, limit, now)
+	lb.mu.Unlock()
+
+	return tokens, exists, ok
+}
+
+// PutString attempt to add quantity tokens into the bucket referred to the
+// string ks that has a size limit of limit tokens.  Return the current
+// number of tokens in the bucket, whether the key already existed, and
+// whether there was enough room in the bucket to add the requested number
+// of tokens.
+func (lb *Cache) PutString(ks string, quantity, limit int64) (int64, bool, bool) {
+	k := key(ks)
+	now := time.Now().UnixNano()
+
+	lb.mu.Lock()
+	tokens, exists, ok := lb.put(k, quantity, limit, now)
+	lb.mu.Unlock()
+
+	return tokens, exists, ok
+}
+
+func key(k string) uint64 {
+	h := fnv.New64a()
+	h.Write([]byte(k))
+	return h.Sum64()
+}
+
+func (lb *Cache) put(k uint64, quantity, limit, now int64) (int64, bool, bool) {
+	e, exists := lb.cache[k]
+	if exists {
+		e = e.update(now)
+	} else {
+		e = entry{tokens: 0, updated: now}
+	}
+
+	ok := e.tokens+quantity <= limit
+	if ok {
+		e.tokens += quantity
+	}
+	lb.cache[k] = e
+
+	if !exists {
+		lb.entries++
+		if lb.entries > lb.MaxEntries {
+			lb.gc(now)
+		}
+	}
+
+	return e.tokens, exists, ok
+}
+
+// gc frees up space in the map, forcibly if necessary.
+func (lb *Cache) gc(now int64) {
+	left := lb.entries - (lb.MaxEntries - gcMustRemoveEntries)
+
+	// Try freeing up entries in a random part of the map.
+	left -= lb.scan(now, gcScanEntries)
+	if left <= 0 {
+		return
+	}
+
+	// If that failed, try one more time in a different part of the map.
+	left -= lb.scan(now, gcScanEntries)
+	if left <= 0 {
+		return
+	}
+
+	// If that failed, just delete some keys.
+	for k := range lb.cache {
+		delete(lb.cache, k)
+		lb.entries--
+		left--
+		if left <= 0 {
+			return
+		}
+	}
+}
+
+// scan attempts to find buckets which are empty to delete.
+func (lb *Cache) scan(now int64, count int) int {
+	start := lb.entries
+
+	n := 0
+	for k, e := range lb.cache {
+		e = e.update(now)
+
+		if e.tokens <= 0 {
+			delete(lb.cache, k)
+			lb.entries--
+		} else {
+			lb.cache[k] = e
+		}
+
+		n++
+		if n >= count {
+			break
+		}
+	}
+
+	return lb.entries - start
+}
+
+// update drains a bucket according to how much time as elapsed since last update.
+func (e entry) update(now int64) entry {
+	// How many nanoseconds have elapsed since last update?
+	s := now - e.updated
+	if s < 0 {
+		s = 0
+	}
+
+	// Drain one token per elapsed nanosecond.
+	t := e.tokens - s
+	if t < 0 {
+		t = 0
+	}
+
+	return entry{tokens: t, updated: now}
+}
---
+++ leakybucket_test.go
@@ -0,0 +1,139 @@
+package leakybucket
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+var entries = []struct {
+	key   uint64
+	value int64
+}{
+	{1, 1000},
+	{2, 2000},
+	{3, 30000},
+	{4, 400000},
+	{5, 50000000},
+}
+
+func TestPut(t *testing.T) {
+	b := New(10000)
+
+	now := time.Now().UnixNano()
+
+	// Add the test entries
+	for _, e := range entries {
+		tokens, exists, ok := b.put(e.key, e.value, 1000000000, now)
+		assert.Equal(t, e.value, tokens)
+		assert.False(t, exists)
+		assert.True(t, ok)
+	}
+
+	assert.Equal(t, 5, b.entries)
+
+	// Fast-forward time by 500ns, gc, and verify that the entries have
+	// reasonable values.
+	now += 500
+	b.gc(now)
+
+	for _, e := range entries {
+		tokens, exists, ok := b.put(e.key, 1, 1000000000, now)
+		assert.Equal(t, e.value-500+1, tokens)
+		assert.True(t, exists)
+		assert.True(t, ok)
+	}
+
+	// Fast-forward time by another 2000ns, gc, and check that we've
+	// freed 2 entries.
+	now += 2000
+	b.gc(now)
+	assert.Equal(t, 3, b.entries)
+}
+
+func TestPutString(t *testing.T) {
+	b := New(10000)
+
+	_, _, _ = b.PutString("1", 1000, 1000000000)
+	_, _, _ = b.PutString("2", 2000, 1000000000)
+	_, _, _ = b.PutString("2", 500, 1000000000)
+
+	assert.Equal(t, 2, b.entries)
+}
+
+func TestGC(t *testing.T) {
+	b := New(10000)
+
+	now := time.Now().UnixNano()
+
+	// Add one too many entries and make sure we removed gcMustRemoveEntries.
+	for n := 0; n < 10001; n++ {
+		_, _, _ = b.put(uint64(n), int64(1000+n), 1000000000, now)
+	}
+
+	assert.Equal(t, 10000-gcMustRemoveEntries, b.entries)
+
+	// Fast forward 2000ns, add another 200 entries, and make sure we
+	// did more GC.
+	now += 2000
+
+	for n := 10002; n < 10200; n++ {
+		_, _, _ = b.put(uint64(n), int64(1000+n), 1000000000, now)
+	}
+
+	assert.InDelta(t, 9600, b.entries, 200)
+}
+
+func TestMaxEntries(t *testing.T) {
+	c := New(2)
+
+	for _, e := range entries {
+		_, _, _ = c.Put(e.key, e.value, 100000000)
+	}
+
+	// Make sure only the last two entries were kept.
+	assert.Equal(t, 2, c.entries)
+}
+
+func TestRace(t *testing.T) {
+	c := New(4000)
+
+	for worker := 0; worker < 8; worker++ {
+		go testRaceWorker(c)
+	}
+}
+
+func testRaceWorker(c *Cache) {
+	for n := 0; n < 1000; n++ {
+		_, _, _ = c.Put(uint64(rand.Int31n(100)), rand.Int63n(1000000000), 1000000000)
+	}
+}
+
+func TestOverhead(t *testing.T) {
+	if testing.Short() || !testing.Verbose() {
+		t.SkipNow()
+	}
+
+	num := 100000
+	c := New(num)
+
+	now := time.Now().UnixNano()
+
+	mem := readMem()
+
+	for n := 0; n < num*4; n++ {
+		_, _, _ = c.put(uint64(n), int64(n), int64(n), now)
+	}
+
+	mem = readMem() - mem
+	t.Log("entryOverhead =", int64(mem)/int64(num))
+}
+
+func readMem() int64 {
+	m := runtime.MemStats{}
+	runtime.GC()
+	runtime.ReadMemStats(&m)
+	return int64(m.Alloc)
+}
